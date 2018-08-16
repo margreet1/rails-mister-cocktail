857e143d15fc25e29c1fd2af88aa7482ca0f3b9c
Margreet Dijkstra <dijkstra.margreet@gmail.com>
2018-08-16 13:58:27 +0100

    made models for mister cocktail

---
+++ app/models/dose.rb
@@ -0,0 +1,8 @@
+class Dose < ApplicationRecord
+  validates :description, presence: true
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true
+  validates_uniqueness_of :cocktail, scope: :ingredient
+  belongs_to :cocktail
+  belongs_to :ingredient
+end
