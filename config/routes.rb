c48fb2266fb6801fba029e184437e0d2a86b1214
Margreet Dijkstra <dijkstra.margreet@gmail.com>
2018-08-31 13:31:21 +0100

    routing, controllers ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root to: 'cocktails#index'
-    resources :cocktails do
-      resources :doses, only: [:new, :create, :destroy]
-    end
+  root to: "cocktails#index"
+  resources :cocktails, only: [ :index, :show, :new, :create ] do
+    resources :reviews, only: [:new, :create]
+    resources :doses, only: [ :new, :create]
+  end
+  resources :doses, only: [ :destroy]
 end
+#     root to: 'cocktails#index'
+#     resources :cocktails do
+#       resources :doses, only: [:new, :create, :destroy]
+#     end
+# end
+
+
